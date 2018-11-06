c9b49076144eae75cdb486967f8b94d6f26f8b67
Plamen Todorov <plamen@abrites.com>
2018-11-06 10:53:38 +0200

    Initial version

---
+++ gtest.el
@@ -0,0 +1,42 @@
+;;; -*- lexical-binding: t -*-
+(require 'json)
+(load (expand-file-name "orgreport.el"))
+
+;; set the appropriate file name, which can be included as org-agenda file
+(defvar gtest-report "~/org/gtest.org")
+
+(defun gt-output-file()
+  "Temporary file name to be used for the json output"
+  (concat temporary-file-directory (make-temp-name "gtest")))
+
+(defun gt-clear-buffer(buf)
+  (with-current-buffer buf
+    (save-excursion
+      (erase-buffer))))
+
+  
+(defun gt-completed(file-name)
+  "gtest completion callback. Parse the result into the report file, remove the temp json file"
+  (gt-json-to-org (json-read-file file-name)
+               (find-file gtest-report))
+  (delete-file file-name))
+
+
+(defun gt-working-directory(program)
+  (mapconcat #'identity (butlast (split-string program "/")) "/"))
+
+
+(defun gt-start(program)
+  (interactive "sTest program: ")
+  (let ((gtlog (get-buffer-create "*gtlog*"))
+        (json-file-name (gt-output-file))
+        (default-directory (gt-working-directory program)))
+
+    (gt-clear-buffer gtlog)
+    (make-process :name "gtest"
+                  :command (list program (concat "--gtest_output=json:" json-file-name))
+                  :buffer "gtest"
+                  :stderr gtlog
+                  :sentinel #'(lambda(process event)
+                                (gt-completed json-file-name)))))
+(provide 'gtest)
---
+++ orgreport.el
@@ -0,0 +1,57 @@
+(require 'org)
+
+(defconst gt-tokens '(tests failures disabled errors timestamp time status))
+
+(defun gt-as-property(cons)
+  "Convert to org property string"
+  (if (consp cons)
+    (format "  :%s: %s\n" (car cons) (cdr cons))))
+
+(defun gt-summary-property-list(report tokens)
+  "Converts the json properties from the assoc list to string list in org mode property format"
+  (remove-if #'null
+             (mapcar #'(lambda(x) (gt-as-property (assoc x report))) tokens)))
+
+
+(defun gt-append-headline(stars report &optional progress)
+  "Insert org item. Optionally display percent progress of the subitems"
+  (insert stars " " (alist-get 'name report))
+  (when progress
+    (insert progress))
+  (insert "\n  :PROPERTIES:\n")
+  (mapc #'insert (gt-summary-property-list report gt-tokens))
+  (insert "  :END:\n"))
+
+
+(defun gt-append-failure-message(msg)
+  (insert "*** " (alist-get 'failure msg) "\n\n"))
+
+
+(defun gt-append-test-case(case)
+  (insert  "** " (alist-get 'name case))
+  (if (assoc 'failures case)
+      (org-todo '(1)) ;;assumed this is TODO
+    (org-todo '(2)))  ;;assumed this is DONE
+  (insert "\n")
+  (mapc #'gt-append-failure-message (alist-get 'failures case)))
+      
+
+(defun gt-append-group(group)
+  "gtest test suite consists of multiple tests"
+  (gt-append-headline "*" group " [%]")
+  (mapc 'gt-append-test-case (alist-get 'testsuite group)))
+
+(defun gt-append-test-groups(report)
+  (mapc 'gt-append-group report))
+      
+(defun gt-json-to-org(report buffer)
+  (with-current-buffer buffer
+    (save-excursion
+      (erase-buffer)
+      (org-mode)
+      (insert "#+STARTUP: overview\n\n")
+      (gt-append-test-groups (alist-get 'testsuites report))
+      (org-set-startup-visibility)
+      (save-buffer))))
+
+
